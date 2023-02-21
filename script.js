356e91a2b9ac9216b8fe074e8b67a05b09fe73e0
Kameleon21 <kamilrogozinski29@gmail.com>
2023-02-21 14:07:15 +0000

    add basic chosing player info and using factory function to create the object

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const gameBoardContainer = document.querySelector(".gameboard");
+const gameBoardContainer = document.querySelector('.gameboard');
+const startBtn = document.querySelector('.start');
+const resetBtn = document.querySelector('.reset');
+let p1;
+let p2;
 
 /* create gameBoard array inside the gameBoard object */
-
 const game = (function () {
-  let gameBoard = [
-    ["X", "O", "X"],
-    ["O", "X", "O"],
-    ["X", "O", "X"],
+  const gameBoard = [
+    ['X', 'O', 'X'],
+    ['O', 'X', 'O'],
+    ['X', 'O', 'X'],
   ];
 
   return {
@@ -14,23 +17,36 @@ const game = (function () {
   };
 })();
 
-/* create players factory function */
+console.log(game.Gameboard);
 
+/* create players factory function */
 function Player(name, choice) {
   // private functions
-  const _getName = () => name;
-  const _getChoice = () => choice;
+  const getName = () => name;
+  const getChoice = () => choice;
 
-  // public functions 
-  const printName = () => `Nice to meet you ${_getName()}`;
-  const printChoice = () => `You chose ${_getChoice()}`;
-  return {printName, printChoice };
+  // public functions
+  const printName = () => `Nice to meet you ${getName()}`;
+  const printChoice = () => `You chose ${getChoice()}`;
+  return { printName, printChoice };
 }
 
-const p1 = Player("Tom", "O");
-const p2 = Player("Sally", "X");
-
-console.log(p1.printName());
-console.log(p1.printChoice());
-console.log(p2.printName());
-console.log(p2.printChoice());
+// function to loop over until 2 players have been created
+const createPlayers = () => {
+  let playerCount = 1;
+  while (playerCount <= 1) {
+    let p1Name = prompt('Player 1 enter your name');
+    let p1Choice = prompt('Choose X or O');
+    p1 = Player(p1Name, p1Choice);
+    if (p1Choice === 'X') {
+      // what ever p1 one choses p2 will be left with the other option
+      let p2Name = prompt('Player 2 enter your name');
+      p2 = Player(p2Name, 'O');
+    } else if (p1Choice === 'O') {
+      let p2Name = prompt('Player 2 enter your name');
+      p2 = Player(p2Name, 'X');
+    }
+    playerCount++;
+  }
+};
+createPlayers();
