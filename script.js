58232619c40e9bdda4505b5a407e37f45984e9a5
Kameleon21 <kamilrogozinski29@gmail.com>
2023-02-21 07:40:44 +0000

    add gameboard array and factory function to produce player objects

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+const gameBoardContainer = document.querySelector(".gameboard");
 
+/* create gameBoard array inside the gameBoard object */
+
+const game = (function () {
+  let gameBoard = [
+    ["X", "O", "X"],
+    ["O", "X", "O"],
+    ["X", "O", "X"],
+  ];
+
+  return {
+    Gameboard: gameBoard,
+  };
+})();
+
+/* create players factory function */
+
+function Player(name, choice) {
+  // private functions
+  const _getName = () => name;
+  const _getChoice = () => choice;
+
+  // public functions 
+  const printName = () => `Nice to meet you ${_getName()}`;
+  const printChoice = () => `You chose ${_getChoice()}`;
+  return {printName, printChoice };
+}
+
+const p1 = Player("Tom", "O");
+const p2 = Player("Sally", "X");
+
+console.log(p1.printName());
+console.log(p1.printChoice());
+console.log(p2.printName());
+console.log(p2.printChoice());
